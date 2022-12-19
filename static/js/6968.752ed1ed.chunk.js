56b84a2492188e5d9c44194069750eb32e0cbdd0
Nakesto <vinboyhuang88@gmail.com>
2022-12-20 04:05:31 +0700

    first

---
+++ static/js/6968.752ed1ed.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 6968.752ed1ed.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkModa=self.webpackChunkModa||[]).push([[6968],{6968:function(n,u,e){e.r(u),e.d(u,{g:function(){return a}});var a=e(9012).i}}]);
+//# sourceMappingURL=6968.752ed1ed.chunk.js.map
---
+++ static/js/9868.7a634627.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 9868.7a634627.chunk.js.LICENSE.txt */
+"use strict";(self.webpackChunkModa=self.webpackChunkModa||[]).push([[9868],{9868:function(n,e,i){i.r(e),i.d(e,{c:function(){return t}});var o=i(6835),t=function(n){var e,i,t,r=function(){e=function(){t=!0,n&&n(!0)},i=function(){t=!1,n&&n(!1)},null===o.w||void 0===o.w||o.w.addEventListener("keyboardWillShow",e),null===o.w||void 0===o.w||o.w.addEventListener("keyboardWillHide",i)};return r(),{init:r,destroy:function(){null===o.w||void 0===o.w||o.w.removeEventListener("keyboardWillShow",e),null===o.w||void 0===o.w||o.w.removeEventListener("keyboardWillHide",i),e=i=void 0},isKeyboardVisible:function(){return t}}}}}]);
+//# sourceMappingURL=9868.7a634627.chunk.js.map
